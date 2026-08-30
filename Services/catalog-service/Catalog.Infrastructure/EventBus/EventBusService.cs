3c62ee0a657364d063fb36711c3eaffaf7bfe079
reza1370programmer <78476427+reza1370programmer@users.noreply.github.com>
2026-08-30 21:29:06 +0330

    event-bus

---
+++ Services/catalog-service/Catalog.Infrastructure/EventBus/EventBusService.cs
@@ -0,0 +1,25 @@
+﻿
+
+using MassTransit;
+
+namespace Catalog.Infrastructure.EventBus
+{
+    public interface IEventBusService
+    {
+        Task PublishAsync<T>(T message, CancellationToken cancellationToken = default) where T : class;
+    }
+    public class EventBusService : IEventBusService
+    {
+        public readonly IPublishEndpoint _publishEndpoint;
+
+        public EventBusService(IPublishEndpoint publishEndpoint)
+        {
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public async Task PublishAsync<T>(T message, CancellationToken cancellationToken = default) where T : class
+        {
+            await _publishEndpoint.Publish<T>(message, cancellationToken);
+        }
+    }
+}
