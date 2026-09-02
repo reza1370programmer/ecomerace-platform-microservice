ad04432424625ff9fc8aaabc6ae8a75de6c09ecb
reza1370programmer <rezasetareh1370@gmail.com>
2026-09-02 13:34:24 +0330

    add-dtos

---
+++ Services/catalog-service/Catalog.Application/Dto/CategoryDto.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Catalog.Application.Dto
+{
+    public record CategoryDto
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; } = string.Empty;
+        public string Description { get; init; }
+    }
+}
---
+++ Services/catalog-service/Catalog.Application/Dto/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Catalog.Application.Dto
+{
+    public class ProductDto
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+        public string Currency { get; init; }
+        public string ShortDescription { get; init; }
+        public string? LongDescription { get; init; }
+        public string Sku { get; init; }
+        public Guid CategoryId { get; init; }
+        public string CategoryName { get; init; }
+        public DateTime CreateAt { get; init; }
+
+    }
+}
