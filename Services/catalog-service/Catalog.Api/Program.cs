5f695e2fe9d13c392d2912c1fa17d84b6c82bff0
reza1370programmer <rezasetareh1370@gmail.com>
2026-09-12 16:19:35 +0330

    fix

--- Services/catalog-service/Catalog.Api/Program.cs
+++ Services/catalog-service/Catalog.Api/Program.cs
@@ -1,3 +1,11 @@
+using Catalog.Application.Handler;
+using Catalog.Domain.Repositories;
+using Catalog.Infrastructure.EventBus;
+using Catalog.Infrastructure.Persistence.EfCore;
+using Catalog.Infrastructure.Persistence.Repositories;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +14,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CatalogContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMQ:Host"], "/", h =>
+        {
+            h.Username(builder.Configuration["RabbitMQ:Username"]);
+            h.Password(builder.Configuration["RabbitMQ:Password"]);
 
+        });
+        cfg.ConfigureEndpoints(ctx);
+    });
+});
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateProductCommandHandler).Assembly));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IEventBusService, EventBusService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,9 +43,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
+using (var scope = app.Services.CreateScope())
+{
+    var dbcontext = scope.ServiceProvider.GetRequiredService<CatalogContext>();
+    await dbcontext.Database.MigrateAsync();
+}
 app.Run();
